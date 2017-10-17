e41d984050bbf58f08c7ef3fd9062ced88acc856
peynmurray <peynmurray@gmail.com>
2017-10-16 22:28:18 -0700

    Fixed stupid placement of exponential for scientific notation axis labels

--- pycorner.py
+++ pycorner.py
@@ -1,11 +1,34 @@
 import matplotlib
 matplotlib.use("Qt5Agg")
 import matplotlib.pyplot as plt
-import seaborn as sb
 import numpy as np
+import cmocean
+import matplotlib.ticker as ticker
+
+def labelOffset(ax, axis="y"):
+
+	if axis == "y":
+		fmt = ax.yaxis.get_major_formatter()
+		ax.yaxis.offsetText.set_visible(False)
+		labelfunc = ax.set_ylabel
+		label = ax.get_ylabel()
+	elif axis == "x":
+		fmt = ax.xaxis.get_major_formatter()
+		ax.xaxis.offsetText.set_visible(False)
+		labelfunc = ax.set_xlabel
+		label = ax.get_xlabel()
+
+	def updateLabel(lim):
+		labelfunc("{} ({})".format(label, fmt.get_offset()))
+		return
+
+	ax.callbacks.connect("ylim_changed", updateLabel)
+	ax.figure.canvas.draw()
+	updateLabel(None)
+	return
 
 
-def pycorner(samples, bins=40, ranges=None, labels=None, fig=None, cmap=None, plotType="hist"):
+def pycorner(fig, samples, bins=100, ranges=None, labels=None, cmap=None, plotType="hist"):
 	"""
 	samples: 2D array of shape (nsamples, ndim)
 	"""
@@ -31,14 +54,8 @@ def pycorner(samples, bins=40, ranges=None, labels=None, fig=None, cmap=None, pl
 			if range[i] is None:
 				range[i] = [np.nanmin(samples[:,i]), np.nanmax(samples[:,i])]
 
-	# Create a new figure if one wasn't provided.
-	if fig is None:
-		fig, axes = plt.subplots(ndim, ndim, figsize=(ndim, ndim))
-	else:
-		try:
-			axes = np.array(fig.axes).reshape((ndim, ndim))
-		except:
-			raise ValueError("Provided figure has {} axes, but data has dimensions K={}".format(len(fig.axes), ndim))
+	#Divide the figure into a bunch of subplots
+	axes = fig.subplots(ndim, ndim)
 
 	#Set the default colormap to viridis
 	if cmap is None:
@@ -49,7 +66,7 @@ def pycorner(samples, bins=40, ranges=None, labels=None, fig=None, cmap=None, pl
 
 	for i in range(ndim):
 
-		#Get the axes
+		#Plot the 1D histograms along the diagonal
 		ax = axes[i, i]
 		ax.hist(samples[:,i], bins=bins[i], range=ranges[i])
 		ax.set_yticklabels([])
@@ -58,44 +75,50 @@ def pycorner(samples, bins=40, ranges=None, labels=None, fig=None, cmap=None, pl
 			ax.set_xticklabels([])
 		elif i == ndim - 1:
 			ax.set_xlabel(labels[i])
+			ax.get_xaxis().set_major_locator(ticker.MaxNLocator(nbins=5, prune='upper'))
 
+
+		#Plot the 2D histograms in the lower left corner
 		for j in range(ndim):
 
 			if j > i:
 				axes[i,j].axis('off')
-
-
-			if j < i:
+			elif j < i:
 				if plotType in ["hist", "histogram"]:
 					axes[i,j].hist2d(samples[:,j], samples[:,i], bins=[bins[j], bins[i]], range=[ranges[j], ranges[i]], cmap=cmap)
 				elif plotType in ["hex", "hexbin"]:
-					axes[i,j].hexbin(samples[:,j], samples[:,i], gridsize=[bins[j], bins[i]], extent=[*ranges[j], *ranges[i]], cmap=cmap)
+					axes[i,j].hexbin(samples[:,j], samples[:,i], gridsize=[int(0.5*bins[j]), int(0.5*bins[i])], extent=[*ranges[j], *ranges[i]], cmap=cmap)
 				if j == 0:
 					axes[i,j].set_ylabel(labels[i])
+					labelOffset(axes[i,j], "y")
 				else:
 					axes[i,j].set_yticklabels([])
 				if i == ndim - 1:
 					axes[i,j].set_xlabel(labels[j])
+					labelOffset(axes[i,j], "x")
 				else:
 					axes[i,j].set_xticklabels([])
 
-				for tick in axes[i,j].get_xticklabels():
-					tick.set_rotation(45)
-
+				axes[i,j].get_xaxis().set_major_locator(ticker.MaxNLocator(nbins=5, prune='upper'))
+				axes[i,j].get_yaxis().set_major_locator(ticker.MaxNLocator(nbins=5, prune='upper'))
 				xlim, ylim = makeNiceLimits(samples[:,j]), makeNiceLimits(samples[:,i])
-
 				axes[i,j].set_xlim(xlim)
 				axes[i,j].set_ylim(ylim)
+
+			for tick in axes[i,j].get_xticklabels():
+					tick.set_rotation(45)
+
+
 	return
 
-def makeNiceLimits(samplesx, factor=2):
+def makeNiceLimits(samplesx, factor=3):
 	sx = factor*np.std(samplesx)
 	avgx = np.mean(samplesx)
-
-
 	return [avgx-sx, avgx+sx]
 
 if __name__ == "__main__":
+	fig = plt.figure(figsize=(10,10))
+
 	with open("test_data.csv", 'r') as f:
 		rawdata = f.readlines()
 
@@ -105,5 +128,5 @@ if __name__ == "__main__":
 
 	data = np.array(data)
 
-	pycorner(data, labels=["a", "b", "c", "d"], plotType="hex")
+	pycorner(fig, data, labels=["a", "b", "c", "d"], plotType="hist", cmap=cmocean.cm.tempo_r)
 	plt.show()
