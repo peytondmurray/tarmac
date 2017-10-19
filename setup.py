bb17759e6c3091d4277e68923343313b9c7953b7
peytondmurray <peynmurray@gmail.com>
2017-10-19 11:38:49 -0700

    Fixed wrong URL for downloading package

--- setup.py
+++ setup.py
@@ -2,16 +2,36 @@
 # @peytondmurray
 # peytondmurray.github.io
 
-from distutils.core import setup
+from setuptools import setup
 setup(
 	name = 'tarmac',
 	packages = ['tarmac'], # this must be the same as the name above
-	version = '0.1',
+	version = '0.2',
 	description = 'Tools for processing and visualisation markov chain samples',
 	author = 'Peyton Murray',
 	author_email = 'peynmurray@gmail.com',
 	url = 'https://github.com/peytondmurray/tarmac', # use the URL to the github repo
-	download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+	license="GNU GPL 3.0",
+	download_url = 'https://github.com/peytondmurray/tarmac/archive/0.2.zip', # I'll explain this in a second
 	keywords = ['plotting', 'mcmc', 'markov chain', 'monte carlo', "bayesian"], # arbitrary keywords
-	classifiers = [],
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+
+		# Indicate who your project is intended for
+		'Intended Audience :: Developers',
+		'Topic :: Scientific/Engineering :: Visualization',
+
+		# Pick your license as you wish (should match "license" above)
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+		# Specify the Python versions you support here. In particular, ensure
+		# that you indicate whether you support Python 2, Python 3 or both.
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+	],
+	install_requires = ["matplotlib", "numpy", "cmocean"],
+	python_requires = '>=3',
 )
